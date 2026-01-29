7cfafb216935aed0c46f232d42a64a73764dc1c2
unknown <uwaizeben@gmail.com>
2026-01-29 05:05:06 -0800

    fix(Auth): import useEffect in Register

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import API_URL from '../../config';
 
 const Register = ({ onRegister, onSwitchToLogin }) => {
